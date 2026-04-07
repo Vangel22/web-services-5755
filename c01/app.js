756b7857453af77c902349acf3d9156cf35a455a
Vangel22 <h.vangel22@gmail.com>
2026-04-07 21:24:42 +0200

    Save class01

--- c01/app.js
+++ c01/app.js
@@ -4,11 +4,21 @@ require("dotenv").config();
 const connectDb = require("./db/config");
 connectDb();
 
+const {
+  getCars,
+  createCar,
+  updateCar,
+  removeCar,
+} = require("./controllers/cars");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("Hello World!");
-});
+app.use(express.json());
+
+app.get("/cars", getCars);
+app.post("/cars", createCar);
+app.put("/cars/:id", updateCar);
+app.delete("/cars/:kakosakam", removeCar);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server started at port: ${process.env.PORT}`),
--- c01/models/cars.js
+++ c01/models/cars.js
@@ -1,15 +1,69 @@
+const mongoose = require("mongoose");
 // Model za avtomobili
 
 // Baranje: sekoj avtomobil ima:
 // godina, kilometraza, boja, tip na gorivo, karoserija ( karavan, kupe, kabriolet, suv ),
 // tip na menuvac, brend, model.
 
+const carSchema = mongoose.Schema(
+  {
+    brand: String,
+    model: String,
+    year: Number,
+    mileage: Number,
+    paint: String,
+    gasType: {
+      type: String,
+      enum: ["diesel", "petrol", "lpg", "electric"],
+    },
+    isHybrid: Boolean,
+    bodyType: {
+      type: String,
+      enum: ["coupe", "hatchback", "carbio", "SUV"],
+    },
+    transmission: {
+      type: String,
+      enum: ["manual", "automatic"],
+    },
+  },
+  { timestamps: true },
+);
+
+const CarModel = mongoose.model("Car", carSchema, "cars");
+
 // Create
+
+const create = async (carData) => {
+  console.log(carData);
+  const newCar = new CarModel(carData);
+  return await newCar.save();
+};
 
 // Read
 
+const getAll = async () => {
+  return await CarModel.find();
+};
+
 // Update
 
-// Delete
+const update = async (_id, cardData) => {
+  return await CarModel.updateOne({ _id }, carData);
+};
 
-// module.exports -> ke gi koristime vo controllers
+// Delete
+// Ako imeto na vrednosta i klucot se isti, nemora da go povtoruvame _id: _id -> samo _id e dovolno
+// const remove = async (_id) => {
+//   return await CarModel.deleteOne({ _id });
+// };
+
+const remove = async (kakosakam) => {
+  return await CarModel.deleteOne({ _id: kakosakam });
+};
+
+module.exports = {
+  create,
+  update,
+  getAll,
+  remove,
+};
