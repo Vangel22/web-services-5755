18247549cb658f81f7ed36ee1b53f365b6142836
Vangel22 <h.vangel22@gmail.com>
2026-04-09 20:46:53 +0200

    Save v1 of class02

--- c02/v1/handlers/vehicle.js
+++ c02/v1/handlers/vehicle.js
@@ -14,10 +14,55 @@ const createVehicle = async (req, res) => {
   }
 };
 
-const getVehicles = async () => {};
+const getVehicles = async (req, res) => {
+  try {
+    const vehicles = await read("data.json");
+    return res.status(200).send(vehicles);
+  } catch (err) {
+    return res.status(500).send("Internal server error!");
+  }
+};
 
-const updateVehicle = async () => {};
+const updateVehicle = async (req, res) => {
+  try {
+    let vehicles = await read("data.json");
+    const vehicleId = Number(req.params.id); // ova proverka ni treba bidejki index (31) e od tip Number
 
-const removeVehicle = async () => {};
+    vehicles = vehicles.map((vehicle, index) => {
+      if (vehicleId === index) {
+        return {
+          ...vehicle,
+          ...req.body,
+        };
+      }
+      return vehicle; // ako ne go napravime ovoj del ke imame null podatoci vo data.json
+    });
 
-module.exports = {};
+    await write("data.json", vehicles);
+
+    return res.status(200).send(`Vehicle ${vehicleId} updated successfully!`);
+  } catch (err) {
+    return res.status(500).send("Internal server error!");
+  }
+};
+
+const removeVehicle = async (req, res) => {
+  try {
+    let vehicles = await read("data.json");
+    const vehicleId = Number(req.params.id);
+
+    vehicles = vehicles.filter((vehicle, index) => index !== vehicleId);
+
+    await write("data.json", vehicles);
+    return res.status(200).send(`Vehicle ${vehicleId} deleted successfully!`);
+  } catch (err) {
+    return res.status(500).send("Internal server error!");
+  }
+};
+
+module.exports = {
+  createVehicle,
+  updateVehicle,
+  getVehicles,
+  removeVehicle,
+};
--- c02/v1/index.js
+++ c02/v1/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const {
+  getVehicles,
+  createVehicle,
+  updateVehicle,
+  removeVehicle,
+} = require("./handlers/vehicle");
 
 const app = express();
 
 app.use(express.json());
 
 // TODO: Popolnete gi rutite soodvetno so kontrolerite
-app.get();
-app.post();
-app.put();
-app.delete();
+app.get("/vehicles", getVehicles);
+app.post("/vehicles", createVehicle);
+app.put("/vehicles/:id", updateVehicle);
+app.delete("/vehicle/:id", removeVehicle);
 
 app.listen(3000, () => console.log("Server started at port 3000!"));
