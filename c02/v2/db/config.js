38387715325adfbdd02b160e1004450ed66f4b84
Vangel22 <h.vangel22@gmail.com>
2026-04-09 21:59:40 +0200

    removed URI

--- c02/v2/db/config.js
+++ c02/v2/db/config.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const URI =
-  "mongodb+srv://Vangel22:test1234@cluster0.12jzasd.mongodb.net/grupa-5754?appName=Cluster0";
+const URI = "";
 
 async function connectDb() {
   try {
